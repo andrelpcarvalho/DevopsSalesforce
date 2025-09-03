5df526a77d54cc9a950a480fe328b4cfb4b88462
Andre Carvalho <andreluispirescarvalho@gmail.com>
2025-09-03 16:54:37 -0300

    versionamento da pipeline

--- bash/salesforce-build/gerador_package.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Nome do diretório contendo os metadados
-metadata_dir="\Users\andre.carvalho\Documents\BANCO_BV_Workspace_TESTES\build\force-app\\"
-
-# Arquivo package.xml de saída
-output_file="package.xml"
-
-# Inicia o arquivo package.xml
-sf project generate manifest --source-dir $metadata_dir
